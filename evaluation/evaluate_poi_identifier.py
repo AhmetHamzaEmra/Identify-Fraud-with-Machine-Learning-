3ad0e4cdfcec7647ed7b37e2c0dc8dad60de326e
AhmetHamzaEmra <hamza.emra@gmail.com>
2017-02-08 17:14:31 -0600

    updating decision trees

--- evaluation/evaluate_poi_identifier.py
+++ evaluation/evaluate_poi_identifier.py
@@ -25,7 +25,20 @@ data = featureFormat(data_dict, features_list)
 labels, features = targetFeatureSplit(data)
 
 
+### it's all yours from here forward!  
+from sklearn.model_selection import train_test_split
+features_train, features_test, labels_train, labels_test = train_test_split(features, labels, test_size=0.3, random_state=42)
 
-### your code goes here 
+counter=0
+for i in labels_test:
+    if i ==1.0:
+        counter+=1
+print(counter)
+from sklearn.tree import DecisionTreeClassifier
+clf=DecisionTreeClassifier()
+clf = clf.fit(features_train, labels_train)
+pred = clf.predict(features_test)
+print "score: "
+print  clf.score(features_test,labels_test)
 
-
+print float(25/29)
